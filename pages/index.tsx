13c359c8ee2ac98344dc30bf1b50e7d5ed43b310
南汰翼 <taisuke.minami@neuromagic.com>
2024-02-08 21:04:33 +0900

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,70 @@
-import Link from "next/link";
-import Layout from "../components/Layout";
+import { NextPage, GetServerSideProps } from "next";
+import { useEffect, useState } from "react";
+import styles from "./index.module.css"
+import { symlink } from "fs";
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">About</Link>
-    </p>
-  </Layout>
-);
+// getServerSidePropsから渡されるpropsの型
+type Props = {
+    initialImageUrl: string;
+};
 
+const IndexPage: NextPage<Props> = ({ initialImageUrl }) => {
+    const [imageUrl, setImageUrl] = useState(initialImageUrl); // 初期値を渡す
+    const [loading, setLoading] = useState(false); // 初期状態はfalseにしておく
+
+    // ボタンをクリックしたときに画像を読み込む処理
+    const handleClick = async () => {
+        setLoading(true); // 読込中フラグを立てる
+        const newImage = await fetchImage();
+        setImageUrl(newImage.url); // 画像URLの状態を更新する
+        setLoading(false); // 読込中フラグを倒す
+    };
+
+    return (
+        <div className={styles.page}>
+            <button onClick={handleClick} className={styles.button}>他のにゃんこも見る</button>
+            <div className={styles.frame}>{loading || <img src={imageUrl} />}</div>
+        </div>
+    );
+};
 export default IndexPage;
+
+
+// サーバーサイドで実行する処理
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+    const image = await fetchImage();
+    return {
+        props: {
+            initialImageUrl: image.url,
+        },
+    };
+};
+
+
+type Image = {
+    url: string,
+}
+const fetchImage = async (): Promise<Image> => {
+    const res = await fetch("https://api.thecatapi.com/v1/images/search");
+    const images = await res.json();
+    // 配列として表現されているか？
+    if (!Array.isArray(images)) {
+        throw new Error("猫の画像が取得できませんでした");
+    }
+    const image: unknown = images[0];
+    // Imageの構造をなしているか？
+    if (!isImage(image)) {
+        throw new Error("猫の画像が取得できませんでした");
+    }
+    return image;
+};
+
+// 型ガード関数
+const isImage = (value: unknown): value is Image => {
+    // 値がオブジェクトなのか？
+    if (!value || typeof value !== "object") {
+        return false;
+    }
+    // urlプロパティが存在し、かつ、それが文字列なのか？
+    return "url" in value && typeof value.url === "string";
+};
